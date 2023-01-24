d5899f68c3edd9024f98e7153a0a283c5ddf3018
aimanraz <muhamad.aimanrzq@gmail.com>
2023-01-24 13:17:37 +0800

    set navigation option w useLayoutEffect

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,16 +1,34 @@
 // import { useRoute } from "@react-navigation/native";
+
+import { useLayoutEffect } from "react";
 import { View, Text, StyleSheet, FlatList } from "react-native";
 import MealItem from "../components/MealItem";
 
-import { MEALS } from "../data/dummy-data";
+import { MEALS, CATEGORIES } from "../data/dummy-data";
 
-function MealsOverviewScreen({ route }) {
+function MealsOverviewScreen({ route, navigation }) {
   //   const route = useRoute();
   //   const catId = route.params.categoryId;
   const catId = route.params.categoryId;
 
   const displayedMeals = MEALS.filter((mealItem) => {
     return mealItem.categoryIds.indexOf(catId) >= 0;
+  });
+
+  const categoryTitle = CATEGORIES.find(
+    (category) => category.id === catId
+  ).title;
+
+  useLayoutEffect(() => {
+    const categoryTitle = CATEGORIES.find(
+      (category) => category.id === catId
+    ).title;
+    navigation.setOptions({
+      title: categoryTitle,
+    });
+  }, [navigation, catId]);
+  navigation.setOptions({
+    title: categoryTitle,
   });
 
   function renderMealItem(itemData) {
